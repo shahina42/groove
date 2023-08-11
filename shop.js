1b02e266db888323429ad2fedad16525a5c0ef8f
shahina42 <141030593+shahina42@users.noreply.github.com>
2023-08-11 09:47:20 -0700

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,7 @@
+var variable_name=document.querySelector(".btn");
+var act_btn=document.querySelector("#numb");
+
+
+act_btn.onclick=()=>{
+    variable_name.innerText= "Thank You For Shopping With Us.";
+}
